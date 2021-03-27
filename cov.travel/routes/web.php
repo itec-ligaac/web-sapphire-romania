8b506abc2c8a1065f2e1ec38b92f44ebd5e8fb18
sarcavladsapphire <sarca.vlad@sapphiregroup.ro>
2021-03-27 13:37:22 +0200

    Changed the base route.

--- cov.travel/routes/web.php
+++ cov.travel/routes/web.php
@@ -13,11 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+Voyager::routes();
